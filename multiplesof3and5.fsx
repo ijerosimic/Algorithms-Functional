6db80b602dac886c4570b7bb6c4e0fa53ad9e63b
ijerosimic <igor.jerosimic1@gmail.com>
2020-01-10 14:59:30 +0100

    Multiples of 3 and 5

--- multiplesof3and5.fsx
+++ multiplesof3and5.fsx
@@ -9,14 +9,3 @@ let multiplesOf3And5 num =
         match x with
         | _ when isMultiple x -> x
         | _ -> 0)
-
-multiplesOf3And5 (49)
-// let multiplesOf3And5 num =
-//     [ 1 .. num ]
-//     |> List.fold (fun acc x ->
-//         match x with
-//         | 3
-//         | 5 -> acc + x
-//         | _ when x % 3 = 0 -> acc + x
-//         | _ when x % 5 = 0 -> acc + x
-//         | _ -> acc)
